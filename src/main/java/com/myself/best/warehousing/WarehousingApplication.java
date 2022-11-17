7452673e405ae3b137613af103f2eceaa6562bd9
KevinChen <yemai271@gmail.com>
2022-11-18 02:13:45 +0800

    添加配置文件和启动类

---
+++ src/main/java/com/myself/best/warehousing/WarehousingApplication.java
@@ -0,0 +1,14 @@
+package com.myself.best.warehousing;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class WarehousingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WarehousingApplication.class, args);
+        log.info("项目启动成功！");
+    }
+}
