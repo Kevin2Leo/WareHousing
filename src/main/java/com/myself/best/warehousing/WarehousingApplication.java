349c3ff91df9773ad695a88e367d1c1408aba55a
KevinChen <yemai271@gmail.com>
2022-11-19 18:33:07 +0800

    后台员工模块

--- src/main/java/com/myself/best/warehousing/WarehousingApplication.java
+++ src/main/java/com/myself/best/warehousing/WarehousingApplication.java
@@ -3,9 +3,11 @@ package com.myself.best.warehousing;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan //扫描filter
 public class WarehousingApplication {
     public static void main(String[] args) {
         SpringApplication.run(WarehousingApplication.class, args);
--- src/main/java/com/myself/best/warehousing/controller/EmployeeController.java
+++ src/main/java/com/myself/best/warehousing/controller/EmployeeController.java
@@ -1,19 +1,20 @@
 package com.myself.best.warehousing.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.myself.best.warehousing.common.R;
 import com.myself.best.warehousing.entity.Employee;
 import com.myself.best.warehousing.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -24,7 +25,7 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     /**
-     * 员工登录
+     * 后台员工登录
      * @param request
      * @param employee
      * @return
@@ -41,7 +42,7 @@ public class EmployeeController {
         Employee emp = employeeService.getOne(wrapper);
 //        3、如果没有查询到则返回登录失败结果
         if (emp == null) {
-            return R.error("该emp = {Employee@8273} \"Employee(id=1, username=admin, name=管理员, password=e10adc3949ba59abbe56e057f20f883e, phone=13812312312, sex=1, idNumber=110101199001010047, status=1, createTime=2021-05-06T17:20:07, updateTime=2022-05-24T18:16:54, createUser=1, updateUser=1)\"用户不存在！");
+            return R.error("用户不存在！");
         }
 //        4、密码比对，如果不一致则返回登录失败结果
         if (!password.equals(emp.getPassword())) {
@@ -55,6 +56,93 @@ public class EmployeeController {
 //        6、登录成功，将员工id存入Session并返回登录成功结果
         request.getSession().setAttribute("employee", emp.getId());
         return R.success(emp);
+    }
+
+    /**
+     * 后台员工退出
+     * @param request
+     * @return
+     */
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request) {
+        request.getSession().removeAttribute("employee");
+        return R.success("成功退出");
+    }
+
+    /**
+     * 新增员工
+     * @param employee
+     * @return
+     */
+    @PostMapping
+    public R<String> save(HttpServletRequest request, @RequestBody Employee employee) {
+        log.info("新增员工信息：{}", employee.toString());
+
+        //设置初始密码123456，需要进行md5加密处理
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        //获取当前用户id
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+
+        employeeService.save(employee);
+        return R.success("新增员工成功！");
+    }
+
+    /**
+     * 员工信息分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(Integer page, Integer pageSize, String name){
+
+        //构造分页构造器
+        Page pageInfo = new Page(page, pageSize);
+
+        //构造条件构造器
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        //添加过滤条件
+        queryWrapper.like(StringUtils.isNotEmpty(name), Employee::getName, name);
+        //添加排序条件
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+
+        //执行查询
+        employeeService.page(pageInfo, queryWrapper);//自动会把结果分装给pageInfo
+
+        return R.success(pageInfo);
+    }
+
+    /**
+     * 根据id修改用户信息
+     * @param employee
+     * @return
+     */
+    @PutMapping
+    public R<String> update(HttpServletRequest request, @RequestBody Employee employee) {
+
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser((Long) request.getSession().getAttribute("employee"));
+
+        employeeService.updateById(employee);
+        return R.success("员工信息修改成功");
+    }
+
+    /**
+     * 根据id获取员工信息
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public R<Employee> getById(@PathVariable("id") Long id) {
+        Employee employee = employeeService.getById(id);
+        return R.success(employee);
     }
 
 }
